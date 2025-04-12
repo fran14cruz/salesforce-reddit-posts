a0dcf4675ed520f7ece9a8efee9ebde0d3919b13
fran14cruz <fran14.ich@gmail.com>
2025-04-12 17:59:48 -0500

    LWC to display images

---
+++ force-app/main/default/lwc/imageDisplay/imageDisplay.js
@@ -0,0 +1,24 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+
+// Define the field you want to retrieve
+const THUMBNAIL_FIELD = 'Reddit_Post__c.Thumbnail__c';
+
+export default class DisplayRedditImage extends LightningElement {
+    @api recordId; // This gets the current record ID when used in a record page
+    thumbnailUrl;
+    error;
+
+    // Use the wire service to get the record and its fields
+    @wire(getRecord, { recordId: '$recordId', fields: [THUMBNAIL_FIELD] })
+    wiredRecord({ error, data }) {
+        if (data) {
+            // Access the Thumbnail__c field
+            this.thumbnailUrl = data.fields.Thumbnail__c.value;console.log('HERE', this.thumbnailUrl);
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.thumbnailUrl = undefined;
+        }
+    }
+}
