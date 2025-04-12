fdfc91882ef1ef473ad7f88235efc8126897c873
fran14cruz <fran14.ich@gmail.com>
2025-04-12 18:00:07 -0500

    LWC datatable for reddit posts

---
+++ force-app/main/default/lwc/redditPosts/redditPosts.js
@@ -0,0 +1,32 @@
+import { LightningElement, wire } from 'lwc';
+import getRedditPosts from '@salesforce/apex/RedditPostService.getRedditPosts';
+
+const COLUMNS = [
+  { label: 'Title', fieldName: 'Title__c' },
+  { label: 'Author', fieldName: 'Author__c' },
+  {
+      label: 'Thumbnail',
+      fieldName: 'Thumbnail__c',
+      type: 'url',
+      target: "_blank",
+      typeAttributes: {
+        target: '_blank'
+    }
+
+  },
+  { label: 'Text', fieldName: 'Selftext__c' }
+];
+
+export default class RedditPosts extends LightningElement {
+  posts;
+  columns = COLUMNS;
+
+  @wire(getRedditPosts)
+  wiredPosts({ error, data }) {
+      if (data) {
+          this.posts = data;
+      } else if (error) {
+          console.error(error);
+      }
+  }
+}
